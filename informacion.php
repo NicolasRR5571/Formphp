0306bda857d6e58cde16946a9420a53bf6336a8c
NicolasRR5571 <79017440+NicolasRR5571@users.noreply.github.com>
2025-09-13 09:24:36 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -3,6 +3,8 @@ $nombre = $_POST['nombre'];
 $edad = $_POST['edad'];
 $direccion = $_POST['direccion'];
 $salario = $_POST['salario'];
+$email = $_POST['email'];
+$cargo = $_POST['cargo'];
 
 echo "<h2>Informacion recibida</h2>";
 echo "El empleado $nombre tiene $edad años.<br>";
